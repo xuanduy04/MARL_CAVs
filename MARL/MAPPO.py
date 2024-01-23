4a5a7380cca7108a57b7b300f4b51bfac672c797
Xuan Duy <moiduy04@gmail.com>
2024-01-23 22:25:55 +0700

    modify video recorder code.

--- MARL/MAPPO.py
+++ MARL/MAPPO.py
@@ -312,7 +312,7 @@ class MAPPO:
 
             if video_filename is not None:
                 # imageio.mimsave(video_filename, [np.array(frame) for i, frame in enumerate(Recorded_frames)], fps=5)
-                writer = imageio.get_writer('test.mp4', fps=20)
+                writer = imageio.get_writer(video_filename, fps=20)
                 for frame in Recorded_frames:
                     writer.append_data(np.array(frame))
                 writer.close()
