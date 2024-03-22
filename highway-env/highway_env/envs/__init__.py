fb5fca825cff1b408715f46fa3a03004fdcd5dff
Xuan Duy <moiduy04@gmail.com>
2024-03-22 08:00:57 +0700

    Change env imports

--- highway-env/highway_env/envs/__init__.py
+++ highway-env/highway_env/envs/__init__.py
@@ -1,3 +1,3 @@
-from highway_env.envs.merge_env_v1 import *
-from highway_env.envs.merge_priority_env_v0 import *
+# from highway_env.envs.merge_env_v1 import *
+# from highway_env.envs.merge_priority_env_v0 import *
 from highway_env.envs.merge_multilane_priority_env_v0 import *
