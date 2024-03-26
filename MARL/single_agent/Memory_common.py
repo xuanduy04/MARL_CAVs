9c51c7e81f50c86847c6814b578e339611d12ae1
Xuan Duy <moiduy04@gmail.com>
2024-03-26 10:08:45 +0700

    Memory testing.

--- MARL/single_agent/Memory_common.py
+++ MARL/single_agent/Memory_common.py
@@ -6,58 +6,19 @@ Experience = namedtuple("Experience",
                         ("states", "actions", "rewards", "next_states", "dones"))
 
 
-class OnPolicyReplayMemory(object):
-    """
-    Replay memory buffer
-    """
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def _push_one(self, state, action, reward, next_state=None, done=None):
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Experience(state, action, reward, next_state, done)
-        self.position = (self.position + 1) % self.capacity
-
-    def push(self, states, actions, rewards, next_states=None, dones=None):
-        if isinstance(states, list):
-            if next_states is not None and len(next_states) > 0:
-                for s,a,r,n_s,d in zip(states, actions, rewards, next_states, dones):
-                    self._push_one(s, a, r, n_s, d)
-            else:
-                for s,a,r in zip(states, actions, rewards):
-                    self._push_one(s, a, r)
-        else:
-            self._push_one(states, actions, rewards, next_states, dones)
-
-    def sample(self, batch_size):
-        if batch_size > len(self.memory):
-            batch_size = len(self.memory)
-        transitions = random.sample(self.memory, batch_size)
-        batch = Experience(*zip(*transitions))
-
-        # reset the memory
-        self.memory = []
-        self.position = 0
-        return batch
-
-    def __len__(self):
-        return len(self.memory)
-
-
 class ReplayMemory(object):
     """
     Replay memory buffer
     """
-
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
         self.position = 0
 
     def _push_one(self, state, action, reward, next_state=None, done=None):
+        if len(self.memory):
+            assert self.memory[-1].states.shape == state.shape, \
+            f"last_state's shape != new_state's shape\n{self.memory[-1].states.shape} != {state.shape}"
         if len(self.memory) < self.capacity:
             self.memory.append(None)
         self.memory[self.position] = Experience(state, action, reward, next_state, done)
@@ -84,3 +45,19 @@ class ReplayMemory(object):
 
     def __len__(self):
         return len(self.memory)
+
+
+class OnPolicyReplayMemory(ReplayMemory):
+    """
+    Replay memory buffer
+    """
+    def __init__(self, capacity):
+        super.__init__(capacity)
+
+    def sample(self, batch_size):
+        batch = super.sample(batch_size=batch_size)
+
+        # reset the memory
+        self.memory = []
+        self.position = 0
+        return batch
