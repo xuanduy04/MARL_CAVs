04ea11648266d80485e4e48f4fb52cc4baace683
Xuan Duy <moiduy04@gmail.com>
2024-05-31 21:28:46 +0700

    Add seed setting for train_utils

--- MARL_redux/utils/train_utils.py
+++ MARL_redux/utils/train_utils.py
@@ -1,3 +1,21 @@
-def init_env(env, config):
+import numpy as np
+import torch
+
+import random
+
+
+from highway_env.envs import AbstractEnv
+from config import Config
+
+def init_env(env: AbstractEnv, config: Config) -> AbstractEnv:
     env.config['flatten_obs'] = 'use_attention_module' not in config
     return env
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
