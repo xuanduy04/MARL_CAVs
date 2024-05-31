061e3fe7f9e8e8fa1360839d3cdbd0cdba61089d
Xuan Duy <moiduy04@gmail.com>
2024-05-31 21:30:19 +0700

    Change main.py to fit current redux model configs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import numpy as np
 import torch
 
+import random
+
 import gym
 import argparse
 import logging
 from datetime import datetime
 
 from MARL.common.utils import init_dir
-from MARL_redux.utils.train_utils import init_env
+from MARL_redux.utils.train_utils import init_env, set_seed
 from config import import_config
 
 from MARL_redux.model import IPPO
@@ -15,8 +17,7 @@ from MARL_redux.model import IPPO
 
 def train(args):
     config = import_config(args.model)
-    # set seed
-
+    set_seed(config.seed)
 
     # update configs
     config.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -36,32 +37,22 @@ def train(args):
     config.env.action_dim = env_train.action_dim
 
     # init model
-    on_policy = True
-    if on_policy:
-        config.model.num_steps = None
-    policy = IPPO(config.model)
+    policy = IPPO(config)
 
     # Training loop
+    results = []
     for episode in range(0, config.model.train_episodes):
-        num_CAV, num_HDV = config.env.num_CAV, config.env.num_HDV
-        if episode < config.model.curriculum_episodes:
-            # Simulates curriculum training
-            num_CAV = np.random.choice(np.arange(max(min(3, num_CAV), num_CAV - 2), num_CAV + 1), 1)[0]
-            num_HDV = np.random.choice(np.arange(max(1, num_HDV - 2), num_HDV + 1), 1)[0]
-
         # Interacts and trains the policy
-        policy.train(env_train, num_CAV, num_HDV)
+        policy.train(env_train, curriculum_training=episode < config.model.curriculum_episodes)
 
         if (episode + 1) % config.model.eval_interval == 0:
             # evaluate the model
-            result = policy.evaluate(env_eval, num_CAV, num_HDV)
-            print(result)
-        results.append(result)
-        return results  #whatever they are
-
-    policy.save_model()
+            eval_result, _ = policy.evaluate(env_eval, output_dir, episode)
+            print(f"Episode {episode + 1}: {eval_result}")
+        results.append(eval_result)
     # Save the model.
-    pass
+    policy.save_model()
+    return results  # whatever they are
 
 def parse_args():
     default_base_dir = "./results/"
