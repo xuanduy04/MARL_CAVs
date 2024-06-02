3958cd1c1f4b9ded40668ed86a7d6fe89076e9c3
Xuan Duy <moiduy04@gmail.com>
2024-06-02 23:29:36 +0700

    Streamline network.py

--- MARL_redux/common/network.py
+++ MARL_redux/common/network.py
@@ -6,10 +6,10 @@ from torch import nn
 from torch import Tensor
 from torch.distributions.categorical import Categorical
 
-from MARL_redux.utils.debug_utils import checknan
+from MARL_redux.utils.debug_utils import checknan, checknan_Sequential, analyze
 
 
-def layer_init(layer: nn.Linear, method: str = 'orthogonal', **kwargs) -> nn.Linear:
+def layer_init(layer: nn.Linear, method: str = 'xavier', **kwargs) -> nn.Linear:
     if method == 'xavier':
         nn.init.xavier_uniform_(layer.weight)
     elif method == 'orthogonal':
@@ -23,6 +23,7 @@ def layer_init(layer: nn.Linear, method: str = 'orthogonal', **kwargs) -> nn.Lin
 
 class ActorCriticNetwork(nn.Module):
     """An actor critic network, similar to that of cleanrl"""
+
     def __init__(self, state_dim: int, action_dim: int, hidden_size: int):
         super(ActorCriticNetwork, self).__init__()
         self.actor = nn.Sequential(
@@ -30,7 +31,7 @@ class ActorCriticNetwork(nn.Module):
             nn.Tanh(),
             layer_init(nn.Linear(hidden_size, hidden_size)),
             nn.Tanh(),
-            layer_init(nn.Linear(hidden_size, action_dim), std=0.01),
+            layer_init(nn.Linear(hidden_size, action_dim)),
             nn.Tanh(),
         )
 
@@ -39,41 +40,22 @@ class ActorCriticNetwork(nn.Module):
             nn.Tanh(),
             layer_init(nn.Linear(hidden_size, hidden_size)),
             nn.Tanh(),
-            layer_init(nn.Linear(hidden_size, 1), std=1.0)
+            layer_init(nn.Linear(hidden_size, 1))
         )
 
     def get_value(self, state: Tensor) -> Tensor:
         return self.critic(state)
 
     def get_action_and_value(self, state: Tensor, action: Optional[Tensor] = None):
-        if checknan(Input=state, print_when_false=True):
-          print(state)
-          exit(0)
-        nn_check_nan(self.actor)
-        nn_check_nan(self.critic)
+        # if checknan(Input=state, print_when_false=True):
+        #     print(state)
+        #     exit(0)
+        # checknan_Sequential(self.actor)
+        # checknan_Sequential(self.critic)
         logits = self.actor(state)
-        analyze(logits)
+        # analyze(logits)
         probs = Categorical(logits=logits)
         if action is None:
             # Sample an action
             action = probs.sample()
         return action, probs.log_prob(action), probs.entropy(), self.critic(state)
-
-
-def analyze(tensor: Tensor):
-    positive_count, negative_count, zero_count, nan_count = torch.sum(tensor > 0).item(), torch.sum(tensor < 0).item(), torch.sum(tensor == 0).item(), torch.sum(torch.isnan(tensor)).item()
-    print(positive_count, negative_count, zero_count, nan_count)
-
-def nn_check_nan(network: nn.Linear):
-    contains_nan = False
-    for name, param in network.named_parameters():
-        if torch.isnan(param).any():
-            contains_nan = True
-            print(f"Parameter {name} contains NaNs")
-        if param.grad is not None and torch.isnan(param.grad).any():
-            contains_nan = True
-            print(f"Gradient of {name} contains NaNs")
-    if contains_nan is False:
-        print(f"No parameter contains NaNs")
-    else:
-        exit(0)
