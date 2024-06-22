354c0c9b70abb4eaf2e45afcac153b177d4b159e
Xuan Duy <moiduy04@gmail.com>
2024-06-22 20:07:18 +0700

    remove bugged loss amount

--- MARL/model/MAPPO_attention.py
+++ MARL/model/MAPPO_attention.py
@@ -179,58 +179,57 @@ class MAPPO_attention(BaseModel):
         b_returns = returns.reshape(-1)
         b_values = values.reshape(-1)
 
-        for agent_id in range(num_CAV):
-            # Optimizing the agent's policy and value network
-            for epoch in range(args.update_epochs):
-                # printd(f'Epoch {epoch}:')
-                np.random.shuffle(b_inds)
-                for start in range(0, batch_size, minibatch_size):
-                    end = min(start + minibatch_size, batch_size)
-                    mb_inds = b_inds[start:end]
+        # Optimizing the agent's policy and value network
+        for epoch in range(args.update_epochs):
+            # printd(f'Epoch {epoch}:')
+            np.random.shuffle(b_inds)
+            for start in range(0, batch_size, minibatch_size):
+                end = min(start + minibatch_size, batch_size)
+                mb_inds = b_inds[start:end]
 
-                    _, newlogprob, entropy, newvalue = \
-                        self.network.get_action_and_value(b_obs[mb_inds], b_actions.long()[mb_inds])
-                    logratio = newlogprob - b_logprobs[mb_inds]
-                    ratio = logratio.exp()
+                _, newlogprob, entropy, newvalue = \
+                    self.network.get_action_and_value(b_obs[mb_inds], b_actions.long()[mb_inds])
+                logratio = newlogprob - b_logprobs[mb_inds]
+                ratio = logratio.exp()
 
-                    with torch.no_grad():
-                        # calculate approx_kl http://joschu.net/blog/kl-approx.html
-                        old_approx_kl = (-logratio).mean()
-                        approx_kl = ((ratio - 1) - logratio).mean()
+                with torch.no_grad():
+                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                    old_approx_kl = (-logratio).mean()
+                    approx_kl = ((ratio - 1) - logratio).mean()
 
-                    mb_advantages = b_advantages[mb_inds]
-                    if args.norm_adv:
-                        mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
+                mb_advantages = b_advantages[mb_inds]
+                if args.norm_adv:
+                    mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
 
-                    # Policy loss
-                    pg_loss1 = -mb_advantages * ratio
-                    pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef,
-                                                            1 + args.clip_coef)
-                    pg_loss = torch.max(pg_loss1, pg_loss2).mean()
+                # Policy loss
+                pg_loss1 = -mb_advantages * ratio
+                pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef,
+                                                        1 + args.clip_coef)
+                pg_loss = torch.max(pg_loss1, pg_loss2).mean()
 
-                    # Value loss
-                    newvalue = newvalue.view(-1)
-                    if args.clip_vloss:
-                        v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
-                        v_clipped = b_values[mb_inds] + torch.clamp(
-                            newvalue - b_values[mb_inds],
-                            -args.clip_coef,
-                            args.clip_coef,
-                        )
-                        v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
-                        v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
-                        v_loss = 0.5 * v_loss_max.mean()
-                    else:
-                        v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
+                # Value loss
+                newvalue = newvalue.view(-1)
+                if args.clip_vloss:
+                    v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
+                    v_clipped = b_values[mb_inds] + torch.clamp(
+                        newvalue - b_values[mb_inds],
+                        -args.clip_coef,
+                        args.clip_coef,
+                    )
+                    v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
+                    v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
+                    v_loss = 0.5 * v_loss_max.mean()
+                else:
+                    v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
 
-                    entropy_loss = entropy.mean()
-                    loss = pg_loss + args.vf_coef * v_loss - args.ent_coef * entropy_loss
+                entropy_loss = entropy.mean()
+                loss = pg_loss + args.vf_coef * v_loss - args.ent_coef * entropy_loss
 
-                    self.optimizer.zero_grad()
-                    loss.backward()
-                    nn.utils.clip_grad_norm_(self.network.parameters(), args.max_grad_norm)
-                    self.optimizer.step()
-                    self.scheduler.step(loss)
+                self.optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(self.network.parameters(), args.max_grad_norm)
+                self.optimizer.step()
+                self.scheduler.step(loss)
 
             overall_losses.append(loss.item())
             v_losses.append(v_loss.item())
