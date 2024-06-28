c06cef45bfff01f0ac375f2a9a3e230a37aa4d89
Xuan Duy <moiduy04@gmail.com>
2024-06-29 00:24:01 +0700

    adds IPPO_attn_patience

--- MARL/model/__init__.py
+++ MARL/model/__init__.py
@@ -5,3 +5,4 @@ from .MAPPO import MAPPO
 from .MAA2C import MAA2C
 from .MAPPO_attention import MAPPO_attention
 from .IPPO_attention import IPPO_attention
+from .IPPO_attention_patience import IPPO_attention_patience
