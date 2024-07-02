6af24125a10302298b54f904d15e621b16b4a7fb
Xuan Duy <moiduy04@gmail.com>
2024-07-02 14:41:06 +0700

    remove aggressive algs from the model pool

--- MARL/utils/model_utils.py
+++ MARL/utils/model_utils.py
@@ -12,11 +12,11 @@ model_list = {
 
     'mappo_attention': MAPPO_attention,
     'mappo_attention_patience': MAPPO_attention_patience,
-    'mappo_attention_patience_aggressive': MAPPO_attention_patience_aggressive,
+    # 'mappo_attention_patience_aggressive': MAPPO_attention_patience_aggressive,
 
     'ippo_attention': IPPO_attention,
     'ippo_attention_patience': IPPO_attention_patience,
-    'ippo_attention_patience_aggressive': IPPO_attention_patience_aggressive,
+    # 'ippo_attention_patience_aggressive': IPPO_attention_patience_aggressive,
 }
 
 
