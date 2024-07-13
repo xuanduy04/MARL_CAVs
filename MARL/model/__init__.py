839c9c724e2a781b3e691e9fe6a246cd151ce334
Xuan Duy <moiduy04@gmail.com>
2024-07-13 18:40:56 +0700

    remove unused files

--- MARL/model/__init__.py
+++ MARL/model/__init__.py
@@ -1,13 +1,6 @@
 from .BaseModel import BaseModel
 from .IPPO import IPPO
-# from .MADDPG import MADDPG
 from .MAPPO import MAPPO
 from .MAA2C import MAA2C
 
-from .MAPPO_attention import MAPPO_attention
 from .MAPPO_attention_patience import MAPPO_attention_patience
-from .MAPPO_attention_patience_aggressive import MAPPO_attention_patience_aggressive
-
-from .IPPO_attention import IPPO_attention
-from .IPPO_attention_patience import IPPO_attention_patience
-from .IPPO_attention_patience_aggressive import IPPO_attention_patience_aggressive
